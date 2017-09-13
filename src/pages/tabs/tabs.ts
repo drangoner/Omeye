f14248bdb29237df16a6506ba4774ee7012ce6c8
JamesMike <1181281178@qq.com>
2017-09-13 15:24:02 +0800

    android

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Tabs } from 'ionic-angular';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { SettingPage } from '../setting/setting';
+import { BackButtonService } from '../../services/backButton.service';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
+  tabRoots : Object[];
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  @ViewChild('myTabs') tabRef: Tabs;
+  constructor(public backButtonService:BackButtonService, private platform:Platform) {
 
-  constructor() {
-
+  	this.tabRoots = [
+  		{
+  			root: HomePage,
+  			tabTitle: 'Home',
+  			tabIcon: 'home'
+  		},
+  		{
+  			root: SettingPage,
+  			tabTitle: 'setting',
+  			tabIcon: 'person'
+  		}
+  	];
+  	platform.ready().then(() => {
+  		this.backButtonService.registerBackButtonAction(this.tabRef);
+  	});
   }
 }
